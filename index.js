b2faabb577f2886a551de3d700833c4434b19fe2
Panggigo <121463169+Panggigo@users.noreply.github.com>
2025-03-17 09:06:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const { spawn } = require("child_process");
 
+// Menjalankan Bot Discord
+const discordBot = spawn("node", ["discordBot.js"], { stdio: "inherit" });
+
+// Menjalankan Bot WhatsApp
+const waBot = spawn("node", ["waBot.js"], { stdio: "inherit" });
+
+// Menangani error atau proses keluar
+discordBot.on("close", (code) => {
+  console.log(`Bot Discord berhenti dengan kode: ${code}`);
+});
+waBot.on("close", (code) => {
+  console.log(`Bot WhatsApp berhenti dengan kode: ${code}`);
+});
